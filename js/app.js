db7fa9f974da9a5978378a5eae90da7922550a90
FS Henryk <f.s.henryk@gmail.com>
2023-02-12 20:42:30 -0300

    minor js changes

--- js/app.js
+++ js/app.js
@@ -14,35 +14,38 @@ window.addEventListener('scroll', () => {
 }); 
 
 /////////// HEADER ///////////
-    const headerElement = document.querySelector('header')
+const headerElement = document.querySelector('header')
 
-    function PageScrolled() {
-        if (window.scrollY >= 200){
-            headerElement.classList.add('scrolling')
-    } else {
-            headerElement.classList.remove('scrolling')
-    }};
+function PageScrolled() {
+    if (window.scrollY >= 200){
+        headerElement.classList.add('scrolling')
+} else {
+        headerElement.classList.remove('scrolling')
+}};
 
-    // hamburguer display
-    const hamburgerMenu = document.querySelector('.hamburger')
+// hamburguer display
+const hamburgerMenu = document.querySelector('.hamburger')
+const body = document.querySelector('body')
 
-    hamburgerMenu.addEventListener('click', () => {
-        if(headerElement.classList.contains('mobile')){
-            headerElement.classList.remove('mobile')
-            document.querySelector('body').style.overflow = "visible"
-        }else{
-            headerElement.classList.add('mobile')
-            document.querySelector('body').style.overflow = "hidden"
-        }
-    });
+hamburgerMenu.addEventListener('click', () => {
+    if(headerElement.classList.contains('mobile')){
+        headerElement.classList.remove('mobile')
+        body.style.overflow = "visible"
+    }else{
+        headerElement.classList.add('mobile')
+        body.style.overflow = "hidden"
+    }
+});
 
-    // hamburger options
-    document.querySelectorAll('nav > ul li a').forEach(a => {
-        a.addEventListener('click', () => {
-            headerElement.classList.remove('mobile')
-            document.querySelector('body').style.overflow = "visible"
-        })
-    });
+// hamburger options
+const hamburgerLinks = document.querySelectorAll('nav > ul li a')
+
+hamburgerLinks.forEach(a => {
+    a.addEventListener('click', () => {
+        headerElement.classList.remove('mobile')
+        body.style.overflow = "visible"
+    })
+});
 
 /////////// NAV BUTTONS ///////////
 const navButtons = document.querySelector('ul:nth-of-type(1)')
@@ -97,7 +100,7 @@ function changeToUnselected(index) {
 // port-nav 
 var liButtons = document.querySelectorAll('.port-nav ul li')
 
-document.querySelectorAll('.port-nav ul li').forEach(li => {
+liButtons.forEach(li => {
     li.addEventListener('click', () => {
         if(li.classList.contains('selected')) {
             return
@@ -117,8 +120,10 @@ function resetUnselected() {
 }
 
 //change displayed imgs
+const divTrabalhos = document.querySelectorAll('.trabalhos > div')
+
 function displayed(li){
-    document.querySelectorAll('.trabalhos > div').forEach(div => {
+    divTrabalhos.forEach(div => {
         if(div.classList.contains(li.classList[1])){
             div.classList.remove('hide')
         }else{
@@ -129,42 +134,41 @@ function displayed(li){
 
 /////////// CAROUSEL SECTION ///////////
 // slider
-    const slider = document.querySelector('.carousel-itens');
+const slider = document.querySelector('.carousel-itens');
 
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+let isDown = false;
+let startX;
+let scrollLeft;
 
-    slider.addEventListener('mousedown', (e) =>{
-        isDown = true;
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
+slider.addEventListener('mousedown', (e) =>{
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
 
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-    });
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+});
 
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-    });
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+});
 
-    slider.addEventListener('mousemove', (e) => {
-        if(!isDown) return; //stop the fn from running
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = x - startX;
-        slider.scrollLeft = scrollLeft - walk;
-    });
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return; //stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX;
+    slider.scrollLeft = scrollLeft - walk;
+});
 
 /////////// BACK-TO-TOP BUTTON ///////////
-    const backToTopButton = document.querySelector('.back-to-top')
-    
-    function makeBackToTopVisible() {
-    if (window.scrollY >= 300) {
-        backToTopButton.classList.add('visible')
-    } else {
-        backToTopButton.classList.remove('visible')
-    }
-    };
+const backToTopButton = document.querySelector('.back-to-top')
 
+function makeBackToTopVisible() {
+if (window.scrollY >= 300) {
+    backToTopButton.classList.add('visible')
+} else {
+    backToTopButton.classList.remove('visible')
+}
+};
